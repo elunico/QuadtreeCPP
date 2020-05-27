1335600d5d22df7d0ea81fb1626940efa6d4c76e
Thomas Povinelli <tompov227@gmail.com>
2020-05-27 15:46:34 -0400

    Change point string representation and added description

--- include/Point.h
+++ include/Point.h
@@ -2,6 +2,8 @@
 #define POINT_H
 
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 
 struct Point {
@@ -10,6 +12,12 @@ struct Point {
 
   Point() : x(0), y(0) {}
   Point(double x_, double y_) : x(x_), y(y_) {}
+
+  std::string description() const {
+    std::ostringstream s{};
+    s << "Point(" << x << ", " << y << ")";
+    return s.str();
+  }
 };
 
 std::ostream &operator<<(std::ostream &os, Point const &p);
