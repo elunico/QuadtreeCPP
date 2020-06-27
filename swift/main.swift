b1c8dbefda8516e0d1dcd07ee47b2ece32955e1d
Thomas Povinelli <tompov227@gmail.com>
2020-06-27 13:38:38 -0400

    Operator implementations

--- swift/main.swift
+++ swift/main.swift
@@ -1,17 +1,89 @@
 import Foundation
 
-struct Point: Equatable {
-    public static func == (lhs: Point, rhs: Point) -> Bool {
-        lhs.x == rhs.x && lhs.y == rhs.y
-    }
+infix operator ∈
 
+func ∈(lhs: Point, rhs: Rectangle) -> Bool {
+    (lhs.x > (rhs.x - rhs.width))
+        && (lhs.x < (rhs.x + rhs.width))
+        && (lhs.y > (rhs.y - rhs.height))
+        && (lhs.y < (rhs.y + rhs.height))
+}
+
+func ∈(lhs: Point, rhs: Quadtree) -> Bool {
+    lhs ∈ rhs.bounds
+}
+
+infix operator ⋂
+
+func ⋂(lhs: Rectangle, rhs: Rectangle) -> Bool {
+    !(rhs.x - rhs.width > lhs.x + lhs.width
+        || rhs.x + rhs.width < lhs.x - lhs.width
+        || rhs.y - rhs.height > lhs.y + lhs.height
+        || rhs.y + rhs.height < lhs.y - lhs.height)
+
+}
+
+func ⋂(lhs: Rectangle, rhs: Quadtree) -> Bool {
+    lhs ⋂ rhs.bounds
+}
+
+func ⋂(lhs: Quadtree, rhs: Rectangle) -> Bool {
+    lhs.bounds ⋂ rhs
+}
+
+func ⋂(lhs: Quadtree, rhs: Quadtree) -> Bool {
+    lhs.bounds ⋂ rhs.bounds
+}
+
+
+prefix operator !!!
+prefix func !!! (message: @autoclosure () -> String) -> Never {
+    fatalError(message())
+}
+
+precedencegroup ListTransformOperatorPrecedence {
+    higherThan: ListReductionOperatorPrecedence
+    lowerThan: MultiplicationPrecedence
+    associativity: left
+}
+
+infix operator => : ListTransformOperatorPrecedence
+
+func => <T, R> (lhs: [T], rhs: (T) -> R) -> [R] {
+    return lhs.map(rhs)
+}
+
+infix operator |> : ListTransformOperatorPrecedence
+
+func |> <T>(lhs: [T], rhs: (T) -> Bool) -> [T] {
+    lhs.filter(rhs)
+}
+
+precedencegroup ListReductionOperatorPrecedence {
+    higherThan: AdditionPrecedence
+    associativity: left
+}
+
+infix operator >* : ListReductionOperatorPrecedence
+
+func >* <T, S> (lhs: [T], rhs: (initialValue: S, reduction: (S, T) -> S)) -> S {
+    lhs.reduce(rhs.initialValue, rhs.reduction)
+}
+
+func >* <T> (lhs: [T], rhs: (T, T) -> T) -> T {
+    lhs.reduce(rhs)
+}
+
+extension Array {
+    func reduce(_ reduction: (Element, Element) -> Element) -> Element {
+        var proxy = self
+        return proxy.reduce(proxy.removeFirst(), reduction)
+    }
+}
+
+struct Point: Equatable {
     let x: Double
     let y: Double
-
-    init(x: Double, y: Double) {
-        self.x = x
-        self.y = y
-    }
 
     func distance(to other: Point) -> Double {
         sqrt(pow(other.x - x, 2) + pow(other.y - y, 2))
@@ -19,36 +91,60 @@ struct Point: Equatable {
 }
 
 struct Rectangle: Equatable {
-    public static func == (lhs: Rectangle, rhs: Rectangle) -> Bool {
-        lhs.center == rhs.center && lhs.width == rhs.width && lhs.height == rhs.height
-    }
-
     let center: Point
     let width: Double
     let height: Double
 
-    init(center: Point, width: Double, height: Double) {
-        self.center = center
-        self.width = width
-        self.height = height
-    }
-
     var x: Double { center.x }
     var y: Double { center.y }
+}
 
-    func contains(point other: Point) -> Bool {
-        (other.x > (self.x - self.width))
-            && (other.x < (self.x + self.width))
-            && (other.y > (self.y - self.height))
-            && (other.y < (self.y + self.height))
-    }
+precedencegroup ListAppendPrecedence {
+    lowerThan: FunctionArrowPrecedence
+    associativity: left
+}
 
-    func intersects(rectangle other: Rectangle) -> Bool {
-        !(other.x - other.width > self.x + self.width
-            || other.x + other.width < self.x - self.width
-            || other.y - other.height > self.y + self.height
-            || other.y + other.height < self.y - self.height)
+infix operator <- : ListAppendPrecedence
+
+@discardableResult
+func <- (lhs: Quadtree, rhs: Point) -> Quadtree {
+    if lhs.points.count == lhs.capacity {
+        if !lhs.has_children {
+            lhs.divide()
+        }
+        if rhs ∈ lhs.top_left {
+            lhs.top_left <- rhs
+        } else if rhs ∈ lhs.top_right {
+            lhs.top_right <- rhs
+        } else if rhs ∈ lhs.bottom_left {
+            lhs.bottom_left <- rhs
+        } else if rhs ∈ lhs.bottom_right {
+            lhs.bottom_right <- rhs
+        } else {
+            !!!"Insert could not find a subtree to insert into"
+        }
+    } else {
+        lhs.points <- rhs
     }
+    return lhs
+}
+
+@discardableResult
+func <- <Element>(lhs: inout [Element], rhs: Element) -> [Element] {
+    lhs.append(rhs)
+    return lhs
+}
+
+infix operator <<- : ListAppendPrecedence
+
+func <<- <Element>(lhs: inout [Element], rhs: [Element]) {
+    lhs.append(contentsOf: rhs)
+}
+
+infix operator |&|
+
+func |&| (lhs: Point, rhs: Point) -> Bool {
+    lhs != rhs && lhs.distance(to: rhs) < 3.0
 }
 
 class Quadtree {
@@ -66,55 +162,26 @@ class Quadtree {
         self.capacity = capacity
     }
 
-    func contains(point: Point) -> Bool {
-        bounds.contains(point: point)
-    }
-
-    func intersects(area: Rectangle) -> Bool {
-        bounds.intersects(rectangle: area)
-    }
-
     var has_children: Bool { self.split }
 
-    func insert(point p: Point) {
-        if points.count == capacity {
-            if !has_children {
-                divide()
-            }
-            if top_left.contains(point: p) {
-                top_left.insert(point: p)
-            } else if top_right.contains(point: p) {
-                top_right.insert(point: p)
-            } else if bottom_left.contains(point: p) {
-                bottom_left.insert(point: p)
-            } else if bottom_right.contains(point: p) {
-                bottom_right.insert(point: p)
-            } else {
-                fatalError("Insert could not find a subtree to insert into");
-            }
-        } else {
-            points.append(p);
-        }
-    }
-
     func query(area r: Rectangle) -> [Point] {
-        var found = [Point]()
-        if !self.intersects(area: r) {
-            return found;
+        var found: [Point] = []
+        if !(self ⋂ r) {
+            return found
         } else {
-            found.append(contentsOf: points.filter { r.contains(point: $0) })
+            found.append(contentsOf: points |> { $0 ∈ r })
             if self.has_children {
-                found.append(contentsOf: self.top_left.query(area: r));
-                found.append(contentsOf: self.top_right.query(area: r));
-                found.append(contentsOf: self.bottom_left.query(area: r));
-                found.append(contentsOf: self.bottom_right.query(area: r));
+                found <<- self.top_left.query(area: r)
+                found <<- self.top_right.query(area: r)
+                found <<- self.bottom_left.query(area: r)
+                found <<- self.bottom_right.query(area: r)
             }
-            return found;
+            return found
         }
     }
 
     func points(around point: Point, within radius: Double) -> [Point] {
-        query(area: Rectangle(center: Point(x: point.x, y: point.y), width: radius, height: radius)).filter(point.overlaps)
+        query(area: Rectangle(center: Point(x: point.x, y: point.y), width: radius, height: radius)) |> { point |&| $0 }
     }
 
     func divide() {
@@ -124,72 +191,58 @@ class Quadtree {
             w: self.bounds.width / 2.0,
             h: self.bounds.height / 2.0,
             capacity: self.capacity
-        );
+        )
         self.top_right = Quadtree(
-            x:self.bounds.x + self.bounds.width / 2.0,
-            y:self.bounds.y - self.bounds.height / 2.0,
-            w:self.bounds.width / 2.0,
-            h:self.bounds.height / 2.0,
-            capacity:self.capacity
-        );
+            x: self.bounds.x + self.bounds.width / 2.0,
+            y: self.bounds.y - self.bounds.height / 2.0,
+            w: self.bounds.width / 2.0,
+            h: self.bounds.height / 2.0,
+            capacity: self.capacity
+        )
         self.bottom_left = Quadtree(
-            x:self.bounds.x - self.bounds.width / 2.0,
-            y:self.bounds.y + self.bounds.height / 2.0,
-            w:self.bounds.width / 2.0,
-            h:self.bounds.height / 2.0,
-            capacity:self.capacity
-        );
+            x: self.bounds.x - self.bounds.width / 2.0,
+            y: self.bounds.y + self.bounds.height / 2.0,
+            w: self.bounds.width / 2.0,
+            h: self.bounds.height / 2.0,
+            capacity: self.capacity
+        )
         self.bottom_right = Quadtree(
-            x:self.bounds.x + self.bounds.width / 2.0,
-            y:self.bounds.y + self.bounds.height / 2.0,
-            w:self.bounds.width / 2.0,
-            h:self.bounds.height / 2.0,
-            capacity:self.capacity
-        );
-        self.split = true;
+            x: self.bounds.x + self.bounds.width / 2.0,
+            y: self.bounds.y + self.bounds.height / 2.0,
+            w: self.bounds.width / 2.0,
+            h: self.bounds.height / 2.0,
+            capacity: self.capacity
+        )
+        self.split = true
     }
 
     func clear() {
         points = []
-        self.top_left?.clear();
-        self.top_right?.clear();
-        self.bottom_left?.clear();
-        self.bottom_right?.clear();
-        self.split = false;
-    }
-
-}
-
-extension Point {
-    func overlaps(_ other: Point) -> Bool {
-        self != other && self.distance(to: other) < 3.0
+        self.top_left?.clear()
+        self.top_right?.clear()
+        self.bottom_left?.clear()
+        self.bottom_right?.clear()
+        self.split = false
     }
 }
 
 func main() {
-    // about 2 times slower than rust which itself is approximately 1.5 times slower than c++
-    // this is very likely because I am just unfamiliar with rust and swift compared to c++
-    // perhaps i could be making more optimizations and writing better code in swift and rust
-    // for example when i made Point and Rectangle classes instead of structs it slowed down to
-    // 4 times slower than rust and when I made everything a struct including Quadtree it was about
-    // 3 times slower than rust. However, when Point and Rectangle are structs Quadtree is a class
-    // (which to me seems like a very natural arrangement) performance is--as i said--2x worse
-    let total_points = 20000;
-    let w = 200.0;
-    let h = 200.0;
-    let qt = Quadtree(x: w / 2.0, y: h / 2.0, w: w / 2.0, h: h / 2.0, capacity: 4);
-    var points = [Point]()
+    let total_points = 20000
+    let w = 200.0
+    let h = 200.0
+    let qt = Quadtree(x: w / 2.0, y: h / 2.0, w: w / 2.0, h: h / 2.0, capacity: 4)
+    var points: [Point] = []
 
     for i in 0..<5 {
         for _ in 0..<total_points {
             let x = Double.random(in: 0..<w)
             let y = Double.random(in: 0..<h)
             let p = Point(x: x, y: y)
-            qt.insert(point: p);
-            points.append(p)
+            qt <- p
+            points <- p
         }
 
-        let count = points.map { qt.points(around: $0, within: 10.0).count }.reduce(0, +)
+        let count = points => { qt.points(around: $0, within: 10.0).count } >* (+)
 
         print("Round \(i): Found \(count) overlapping points");
         qt.clear();
